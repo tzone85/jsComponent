0e05bf850865e90abd3de9615960c61feb448d87
tzone85 <tzone85@gmail.com>
2019-05-12 00:07:09 +0200

    created new components, used props, embeded components within each other. Used samentic-ui for the css portion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ComentDetail from './ComentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from "faker";
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div className="ui containter comments">
+            <ApprovalCard>
+                <ComentDetail author='Mncedi' timeAgo={"Today at 4:55"} textComent="Yo hayi ndintswempu kodwa" proPic={faker.image.avatar()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <ComentDetail author='Thando' timeAgo={"Today at 8:55"} textComent="Ola Mqwathi" proPic={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <ComentDetail author='Dishman' timeAgo={"Yesterday at 3:30"} textComent="Yo usiskhokho Thando" proPic={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <ComentDetail author='King-T' timeAgo={"Yesterday at 4:50"} textComent="You've got this Mncedi" proPic={faker.image.avatar()} />
+            </ApprovalCard>
+
+        </div>
+    );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'));
